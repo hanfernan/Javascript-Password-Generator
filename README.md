0eda5401823cd0b407348be01656a810e191267e
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-18 18:41:55 -0400

    intiial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Javascript-Password-Generator
+
+##
---
+++ script.js
@@ -0,0 +1,16 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+func
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
