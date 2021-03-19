a1558b7135a3d2f726864d88d68dab9de37b937a
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-19 19:42:21 -0400

    moved some things around and made small changes

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowerCaseCharacters = [abcdefghijklmnopqrstuvwxyz];
+var upperCaseCharacters = [ABCDEFGHIJKLMNOPQRSTUVWXYZ];
+var numericCharacters = [0123456789];
+var specialCharacters = [!#$%&{|}~];
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -13,7 +17,7 @@ function writePassword() {
 
 function generatePassword() {
 
-  //TODO: Write the code that asks the use about the parameters for their password
+  //TODO: Write the code that asks the user about the parameters for their password
   //TODO: Use those parameters to generate a new password
   //TODO: Return that password
 
@@ -27,36 +31,57 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // create a series of prompts:
-//When you click generate password, first prompt: "How many characters would you like your password to contain?" window.prompt
-//THEN second prompt: Click OK to confirm including special characters
-//Third prompt: Click OK to confirm including numeric characters
-//Fourth prompt: Click OK to confirm lowercase characters
-//Fifth prompt: Click OK to confirm uppercase characters
-//then a dynamically generated password is generated 
-//passowrd must be at least 8 characters (this will prompt an error if <8 >129 is entered for first prompt)
-//clicking cancel on options after that will allow the user to continue (i.e they can opt out of numbers or upper case etc.)
-//can have any combo of number lower case upper case special characters
-
 //these are my prompts
 window.prompt("How many characters would you like your password to contain?");
+if (numOfCharacters <8 || >128) {
+  window.prompt(Password must be between 8 and 128 characters);
+}
 window.confirm("Click OK to confirm including special characters");
 window.confirm("Click OK to confirm including numeric characters");
 window.confirm("Click OK to confirm lowercase characters");
 window.confirm("Click OK to confirm uppercase characters");
 
-//these are the variables assigned to my prompts
-
-var length = window.prompt("How many characters would you like your password to contain?");
+//When you click generate password, first prompt: "How many characters would you like your password to contain?" window.prompt
+var numOfCharacters = window.prompt("How many characters would you like your password to contain?");
+console.log(numOfCharacters);
+//if the user enters less than 8 or more than 128 characters, then an error alert needs to appear
+//THEN second prompt: Click OK to confirm including special characters
 var special = window.confirm("Click OK to confirm including special characters");
+console.log(special);
+//Third prompt: Click OK to confirm including numeric characters
 var numeric = window.confirm("Click OK to confirm including numeric characters");
+//if they hit okay, then password needs to include numeric characters. if they hit cancel, it needs to not include them.
+//Fourth prompt: Click OK to confirm lowercase characters
 var lowerCase = window.confirm("Click OK to confirm lowercase characters");
+//if ok, include, if cancel, exclude
+//Fifth prompt: Click OK to confirm uppercase characters
 var upperCase = window.confirm("Click OK to confirm uppercase characters");
+//if ok, include, if cancel, exclude
+//they must have chosen at least one option. if no options chosen, I need an error message and a restart form option
+//given the above, then a dynamically generated password is generated 
+//the password needs to be generated based on the following: password will be x characters long and consist of a RANDOM combo of the selected characters
+//
 
-//generate password based on the following
-//must be between 8 and 128 characters and contain AT LEAST one character type
-
-if (length<8 && length >129) {
-  return "Password must be more than 8 characters or less than 129 characters";
-}
 
 // else if (length > 8 && length < 129)
+
+//.concat combines multiple arrays
+
+//for loop to get computer to pick random number of variables
+
+//then you have to store this and return it
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
