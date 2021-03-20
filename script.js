d1c0795ad6bacdf14c40f3df8de0c00a139df876
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-20 13:14:39 -0400

    worked on password function

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ var generateBtn = document.querySelector("#generate");
 //Defined Variables
 var lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numericChars = [0,1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numericChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChars = ["!","#","$","%","&","{","|","}","~"];
 var numOfChars = window.prompt("How many characters would you like your password to contain?");
 var special = window.confirm("Click OK to confirm including special characters");
 var numeric = window.confirm("Click OK to confirm including numeric characters");
 var lowerCase = window.confirm("Click OK to confirm lowercase characters");
 var upperCase = window.confirm("Click OK to confirm uppercase characters");
+var passwordContainer = [];
 
 // Write password to the #password input
 function writePassword() {
@@ -28,43 +29,52 @@ function generatePassword() {
   //alert user if character length is <8 or >128
   if (numOfChars < 8 || numOfChars> 128) {
     alert("Password must be between 8 and 128 characters");
-    return ("");
+    return generatePassword;
 
   //ask user remaining character questions
-  } else {
-    console.log (special);
-    console.log (numeric);
-    console.log (lowerCase);
-    console.log (upperCase);
+  }console.log (special);
+  if (special) {
+    passwordContainer = passwordContainer.concat(specialChars);
+  };
+  
+  console.log (numeric);
+  if (numeric) {
+      passwordContainer = passwordContainer.concat(numericChars);
   };
 
+  console.log (lowerCase);
+  if (lowerCase) {
+      passwordContainer = passwordContainer.concat(lowerCaseChars);
+  };
+  
+  console.log (upperCase);
+  if (upperCase) {
+      passwordContainer = passwordContainer.concat(upperCaseChars);
+    } 
+  };
   //everything above this works
   //if user responds "cancel" to all, inform them they must select at least one character type
 
   if(!special && !numeric && !lowerCase && !upperCase) {
     alert("Password must include at least one valid character type");
-    
-    //if all character types are selected
-  } else if (special && numeric && lowerCase && upperCase) {
-    //right now this is just the contactination of all my character arrays
-    special.concat([numeric[,lowerCase[,upperCase]]])
-    //right now this is my starting place for pulling a random index from the concatinated array
-    [Math.floor(Math.random())]
-    //right now this is my starting place for the for loop I will create to create a password of numOfChars length
-    for(i = 0; i <= numOfChars; i++){}
-    //question for tomorrow: do you have to write out every single combination? surely there is a simpler way... 
 
+    //if all character types are selected
+  // } else if (special && numeric && lowerCase && upperCase) {
+    //right now this is just the contactination of all my character arrays
+    // special.concat([numeric[,lowerCase[,upperCase]]])
+    //right now this is my starting place for pulling a random index from the concatinated array
+    // [Math.floor(Math.random())]
+    //right now this is my starting place for the for loop I will create to create a password of numOfChars length
+    // for(i = 0; i <= numOfChars; i++){}
   }
 
-  
   return "Temporary Password";
 };
 
- 
+
 
 // // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 
 
@@ -77,3 +87,5 @@ generateBtn.addEventListener("click", writePassword);
   //TODO: Return that password
 
   //password will be numOfCharacters long made up of a random assortment of special, numeric, lowerCase, and upperCase
+
+ 
