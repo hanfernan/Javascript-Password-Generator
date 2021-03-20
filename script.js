7766534600a0233c02b2f46dbdd19a430871d833
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-19 23:15:23 -0400

    prompts are complete and working but alerts need to be fixed. Also started working on producing password based on user input

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 //Defined Variables
-var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCaseCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numericCharacters = [0,1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialCharacters = ["!","#","$","%","&","{","|","}","~"];
-var numOfCharacters = window.prompt("How many characters would you like your password to contain?");
+var lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numericChars = [0,1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChars = ["!","#","$","%","&","{","|","}","~"];
+var numOfChars = window.prompt("How many characters would you like your password to contain?");
 var special = window.confirm("Click OK to confirm including special characters");
 var numeric = window.confirm("Click OK to confirm including numeric characters");
 var lowerCase = window.confirm("Click OK to confirm lowercase characters");
@@ -21,35 +21,59 @@ function writePassword() {
 
 }
 
+//generate password based on user input
 function generatePassword() {
-  var numOfCharacters = window.prompt("How many characters would you like your password to contain?");
-  console.log(numOfCharacters);
-  if (numOfCharacters < 8 || numOfCharacters> 128) {
+  //prompt user to enter desired number of password characters
+  console.log(numOfChars);
+  //alert user if character length is <8 or >128
+  if (numOfChars < 8 || numOfChars> 128) {
     alert("Password must be between 8 and 128 characters");
+    return ("");
+
+  //ask user remaining character questions
   } else {
     console.log (special);
     console.log (numeric);
     console.log (lowerCase);
     console.log (upperCase);
   };
+
+  //everything above this works
+  //if user responds "cancel" to all, inform them they must select at least one character type
+
+  if(!special && !numeric && !lowerCase && !upperCase) {
+    alert("Password must include at least one valid character type");
+    
+    //if all character types are selected
+  } else if (special && numeric && lowerCase && upperCase) {
+    //right now this is just the contactination of all my character arrays
+    special.concat([numeric[,lowerCase[,upperCase]]])
+    //right now this is my starting place for pulling a random index from the concatinated array
+    [Math.floor(Math.random())]
+    //right now this is my starting place for the for loop I will create to create a password of numOfChars length
+    for(i = 0; i <= numOfChars; i++){}
+    //question for tomorrow: do you have to write out every single combination? surely there is a simpler way... 
+
+  }
+
+  
   return "Temporary Password";
 };
 
-  
+ 
+
+// // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
+//.concat combines multiple arrays
+//for loop to get computer to pick random number of variables
+//then you have to store this and return it
+ 
   //TODO: Write the code that asks the user about the parameters for their password
   //TODO: Use those parameters to generate a new password
   //TODO: Return that password
 
-  
-
-//Call the function
-//console.log(generatePassword);
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-// create a series of prompts:
-//these are my prompts
-// else if (length > 8 && length < 129)
-//.concat combines multiple arrays
-//for loop to get computer to pick random number of variables
-//then you have to store this and return it
+  //password will be numOfCharacters long made up of a random assortment of special, numeric, lowerCase, and upperCase
